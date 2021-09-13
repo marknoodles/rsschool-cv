f109c9c25e6fe561763f1086d70ab6cedb41adf8
marknoodles <dambll@mail.ru>
2021-09-13 17:29:34 +0300

    feat: main-draft-create

---
+++ dist/print.bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/print.js ***!
+  \**********************/
+// export default function printMe() {
+//     console.log('I get cal;;kl;lled from print.js!');
+//   }
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJpbnQuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQSwrQkFBK0IsR0FBRztBQUNsQyxNIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcnNzY2hvb2wtY3YvLi9zcmMvcHJpbnQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gcHJpbnRNZSgpIHtcclxuLy8gICAgIGNvbnNvbGUubG9nKCdJIGdldCBjYWw7O2tsO2xsZWQgZnJvbSBwcmludC5qcyEnKTtcclxuLy8gICB9Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
--- src/print.js
+++ src/print.js
@@ -1,3 +1,3 @@
-export default function printMe() {
-    console.log('I get cal;;kl;lled from print.js!');
-  }
+// export default function printMe() {
+//     console.log('I get cal;;kl;lled from print.js!');
+//   }
